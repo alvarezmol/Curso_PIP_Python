a7295042b538d662fc74c1a846e04f27dc704228
Sebastian Alvarez <alvarezmol@yahoo.es>
2023-05-22 16:36:59 -0500

    Modificacion git ignore

---
+++ charts/chart.py
@@ -0,0 +1,11 @@
+from matplotlib import pyplot as plt
+
+
+def generate_pie_chart():
+    labels = ["A","B","C"]
+    values = [100,200,23]
+
+    fig, ax = plt.subplots()
+    ax.pie(values, labels=labels)
+    plt.savefig("pie.png")
+    plt.close()
